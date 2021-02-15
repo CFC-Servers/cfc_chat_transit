0d170c6f8eb07027a11f2da12479367a28dac009
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-14 22:24:59 -0800

    Add Sentry

--- web/main.go
+++ web/main.go
@@ -3,11 +3,24 @@ package main
 import (
     "flag"
     "log"
+    "time"
     "net/http"
+    "os"
+    "github.com/getsentry/sentry-go"
 )
 
 
 func main() {
+    err := sentry.Init(sentry.ClientOptions{
+        Dsn: os.Getenv("SENTRY_DSN"),
+    })
+
+    if err != nil {
+        log.Fatalf("sentry.Init: %s", err)
+    }
+
+    defer sentry.Flush(2 * time.Second)
+
     addr := flag.String("addr", "0.0.0.0:8080", "http service address")
     flag.Parse()
     //log.SetFlags(0)
