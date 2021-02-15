639e61061c294775a14e4d2abc5543c3bad597e4
plally <11771225+plally@users.noreply.github.com>
2021-02-15 01:48:15 -0500

    go fmt project

--- web/main.go
+++ web/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
 )
 
-
 func main() {
-    addr := flag.String("addr", "0.0.0.0:8080", "http service address")
-    flag.Parse()
-    //log.SetFlags(0)
-    http.HandleFunc("/relay", relay)
+	addr := flag.String("addr", "0.0.0.0:8080", "http service address")
+	flag.Parse()
+	//log.SetFlags(0)
+	http.HandleFunc("/relay", relay)
 
-    go queueGroomer()
-    log.Fatal(http.ListenAndServe(*addr, nil))
+	go queueGroomer()
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
