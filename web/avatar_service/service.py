c62e59a13cb46b3a15c36bfaba1eb78e334e44e0
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-16 09:36:53 -0800

    Fix image overlays

--- web/avatar_service/service.py
+++ web/avatar_service/service.py
@@ -1,33 +1,41 @@
 from PIL import Image, ImageDraw
-from flask import Flask, request
+from flask import Flask, request, send_file
+import os.path
 
 import requests
 
 app = Flask(__name__)
 
-transparent = (255, 255, 255, 0)
+transparent = (0, 0, 0, 0)
+
+base_url = avatarservice.cfcservers.org
 
 @app.route("/outline", methods=["POST"])
 def outline():
-
-    # TODO:
-    #  - Convert received image to RGBA
-    #  - Figure out the circle drawing
-    #  - Place image in directory with a uuid or something, respond with full link to that file
-
     content = request.json
     image_url = content["avatarUrl"]
 
+    image_name = image_url.split("/")[-1] #"example.jpg"
+    image_name = image_name.split(".")[0] #"example"
+
+    avatar_path = f"/avatars/{image_name}.png"
+
+    if os.path.isfile(avatar_path):
+        return send_file(avatar_path, mimetype="image/png")
+
     outline_color = content["outlineColor"] # "255 255 255 255"
-    outline_color = outline_color.split(" ") # [255, 255, 255, 255]
+    outline_color = outline_color.split(" ") # ["255", "255", "255", "255"]
+    outline_color = [int(c) for c in outline_color] # [255, 255, 255, 255]
     outline_color = tuple(outline_color) # (255, 255, 255, 255)
 
     avatar = Image.open(requests.get(image_url, stream=True).raw)
 
     x, y =  avatar.size
-    eX, eY = 30, 60 #Size of Bounding Box for ellipse
-
-    bbox = (x/2 - eX/2, y/2 - eY/2, x/2 + eX/2, y/2 + eY/2)
-    draw = ImageDraw.Draw(avatar)
-    draw.ellipse(bbox, fill=transparent, outline=outline_color, width=25)
+    bbox = (0, 0, x, y)
+    draw = ImageDraw.Draw(avatar, "RGBA")
+    draw.ellipse(bbox, fill=transparent, outline=outline_color, width=8)
     del draw
+
+    avatar.save(avatar_path, "PNG")
+
+    return send_file(avatar_path, mimetype="image/png")
