f65ae3fe05e8d2fa474a80911b48e98c0cdf5070
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-16 13:09:02 -0800

    Change message order

--- web/discord_relay/queue.go
+++ web/discord_relay/queue.go
@@ -35,10 +35,10 @@ func sendMessage(discord *discordgo.Session, message MessageStruct) {
 	joinUrl := "https://cfcservers.org/" + strings.ToLower(message.Realm) + "/join"
 
 	var contentBuilder strings.Builder
-	contentBuilder.WriteString(message.Content)
-	contentBuilder.WriteByte('\n')
 	contentBuilder.WriteString(fmt.Sprintf("[%v](%v)", JOIN_EMOJI, joinUrl))
 	contentBuilder.WriteString(fmt.Sprintf("[%v](%v)", STEAM_EMOJI, profileUrl))
+	contentBuilder.WriteString(" - ")
+	contentBuilder.WriteString(message.Content)
 
 	params := &discordgo.WebhookParams{
 		Content:   contentBuilder.String(),
