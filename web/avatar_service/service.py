f9bce50f66276af3460c78ca75e6595e373eaf44
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-02-17 10:15:07 -0800

    Fix avatars dir, make border thicc

--- web/avatar_service/service.py
+++ web/avatar_service/service.py
@@ -33,7 +33,7 @@ def outline():
     x, y =  avatar.size
     bbox = (0, 0, x, y)
     draw = ImageDraw.Draw(avatar, "RGBA")
-    draw.ellipse(bbox, fill=transparent, outline=outline_color, width=8)
+    draw.ellipse(bbox, fill=transparent, outline=outline_color, width=10)
     del draw
 
     avatar.save(avatar_path, "PNG")
