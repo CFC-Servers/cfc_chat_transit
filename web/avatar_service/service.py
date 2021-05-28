e5bbadad0d7e9aa9b4e9127ef555dff65186d597
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-05-27 17:10:59 -0700

    Update web stuff

--- web/avatar_service/service.py
+++ web/avatar_service/service.py
@@ -1,39 +1,40 @@
 from PIL import Image, ImageDraw
-from flask import Flask, request, send_file
+from flask import Flask, request
 import os
 
 import requests
 
 app = Flask(__name__)
 
-transparent = (0, 0, 0, 0)
-
+TRANSPARENT = (0, 0, 0, 0)
 base_url = os.getenv("AVATAR_SERVICE_URL")
 
+
 @app.route("/outline", methods=["POST"])
-def outline():
+def outline() -> str:
     content = request.json
     image_url = content["avatarUrl"]
 
-    image_name = image_url.split("/")[-1] #"example.jpg"
-    image_name = image_name.split(".")[0] #"example"
+    image_name = image_url.split("/")[-1]  # "example.jpg"
+    image_name = image_name.split(".")[0]  # "example"
 
     avatar_path = f"/avatars/{image_name}.png"
 
     if os.path.isfile(avatar_path):
         os.remove(avatar_path)
 
-    outline_color = content["outlineColor"] # "255 255 255 255"
-    outline_color = outline_color.split(" ") # ["255", "255", "255", "255"]
-    outline_color = [int(c) for c in outline_color] # [255, 255, 255, 255]
-    outline_color = tuple(outline_color) # (255, 255, 255, 255)
+    outline_color = content["outlineColor"]  # "255 255 255 255"
+    outline_color = outline_color.split(" ")  # ["255", "255", "255", "255"]
+    outline_color = [int(c) for c in outline_color]  # [255, 255, 255, 255]
+    outline_color = tuple(outline_color)  # (255, 255, 255, 255)
 
-    avatar = Image.open(requests.get(image_url, stream=True).raw).convert("RGB")
+    raw_image = requests.get(image_url, stream=True).raw
+    avatar = Image.open(raw_image).convert("RGB")
 
-    x, y =  avatar.size
+    x, y = avatar.size
     bbox = (0, 0, x, y)
     draw = ImageDraw.Draw(avatar, "RGBA")
-    draw.ellipse(bbox, fill=transparent, outline=outline_color, width=12)
+    draw.ellipse(bbox, fill=TRANSPARENT, outline=outline_color, width=12)
     del draw
 
     avatar.save(avatar_path, "PNG")
