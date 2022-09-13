6437e64ad782e701bb726cf9c3afcbac71e6cb95
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-09-12 21:45:35 -0700

    Create new voice process queue, expect json from api

--- moon/cfc_chat_transit/server/modules/voice.moon
+++ moon/cfc_chat_transit/server/modules/voice.moon
@@ -1,9 +1,20 @@
 import IsValid from _G
 import guard from ChatTransit
 
-ChatTransit.ReceiveVoiceTranscript = (steamID64, transcript) =>
+local proxVoice
+
+proxVoiceEnabled = ->
+    proxVoice or= GetConVar "force_proximity_voice"
+    return proxVoice\GetBool!
+
+ChatTransit.ReceiveVoiceTranscript = (steamID64, data) =>
+    return if proxVoiceEnabled!
+
     ply = player.GetBySteamID64 steamID64
     return unless IsValid ply
+
+    return if ply.ulx_gagged
+    return if ply\GetInfoNum("proximity_voice_enabled", 0) ~= 0
 
     shouldRelay = hook.Run "CFC_ChatTransit_ShouldRelayTranscript", ply, transcript
     return if shouldRelay == false
@@ -12,7 +23,7 @@ ChatTransit.ReceiveVoiceTranscript = (steamID64, transcript) =>
         Type: "voice_transcript"
         Data:
             Type: "voice_transcript"
-            Content: transcript
+            Content: data
             SteamName: ply\Nick!
             SteamId: ply\SteamID64!
             IrisId: "none"
---
+++ web/discord_relay/voice/voice.go
@@ -0,0 +1,159 @@
+package voice
+
+import (
+	"encoding/json"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"sync"
+	"time"
+)
+
+type Session struct {
+	SteamId   string
+	SteamName string
+	Message   string
+	MessageId string
+	FileName  string
+	Avatar    string
+	Finished  bool
+}
+
+type Operation struct {
+	Session *Session
+}
+
+type Manager struct {
+	Sessions    map[string]*Session
+	Operations  []*Operation
+	discord     *discordgo.Session
+	opmutex     *sync.Mutex
+	sendMessage func(*discordgo.Session, *Session) string
+}
+
+// MessageData The "Content" key of the Event that comes into the Main package
+type MessageData struct {
+	Message   string `json:"transcript"`
+	FileName  string `json:"file_name"`
+	SessionID string `json:"session_id"`
+	IsFinal   bool   `json:"is_final"`
+}
+
+func (v *Manager) runSendQueue() {
+	bucket := v.discord.Ratelimiter.GetBucket("voiceTranscriptions")
+
+	wait := func() {
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	for {
+		wait()
+
+		// This should be a blocking call
+		// Unblocks when bucket has bandwidth
+		v.discord.Ratelimiter.LockBucketObject(bucket).Unlock()
+
+		if len(v.Operations) == 0 {
+			wait()
+			continue
+		}
+
+		v.opmutex.Lock()
+
+		var firstOperation *Operation
+		firstOperation, v.Operations = v.Operations[0], v.Operations[1:]
+
+		session := firstOperation.Session
+		description := session.Message
+		messageId := session.MessageId
+
+		if len(description) == 0 {
+			// What is this? Why are you showing this to me
+			v.opmutex.Unlock()
+			continue
+		}
+
+		messageId = v.sendMessage(v.discord, session)
+
+		if len(messageId) == 0 {
+			// Failed to send/update message, send to back of queue
+			v.Operations = append(v.Operations, firstOperation)
+			v.opmutex.Unlock()
+			continue
+		} else {
+			session.MessageId = messageId
+		}
+
+		v.opmutex.Unlock()
+	}
+}
+
+func (v *Manager) ReceiveVoiceTranscript(steamId string, steamName string, avatar string, data string) {
+	messageData := MessageData{}
+	err := json.Unmarshal([]byte(data), &messageData)
+	if err != nil {
+		log.Printf("Error parsing voice message: %v", err)
+		return
+	}
+
+	newMessage := messageData.Message
+	isFinal := messageData.IsFinal
+	fileName := messageData.FileName
+	sessionId := messageData.SessionID
+
+	session, ok := v.Sessions[sessionId]
+
+	if !ok {
+		session = &Session{
+			SteamId:   steamId,
+			SteamName: steamName,
+			Message:   newMessage,
+			MessageId: "",
+			Finished:  isFinal,
+			Avatar:    avatar,
+			FileName:  fileName,
+		}
+
+		v.Sessions[sessionId] = session
+	} else {
+		session.Message = newMessage
+		session.Finished = isFinal
+		session.FileName = fileName
+	}
+
+	if isFinal {
+		// Final message for this transcription, we can stop tracking it
+		// TODO: do I use delete here?
+		v.Sessions[sessionId] = nil
+	}
+
+	v.opmutex.Lock()
+	defer v.opmutex.Unlock()
+
+	if ok {
+		// Session already existed for sessionID
+		// Do we already have a pending Operation?
+		for _, operation := range v.Operations {
+			if operation.Session == session {
+				// If so, we don't need to add another operation
+				return
+			}
+		}
+	}
+
+	v.Operations = append(v.Operations, &Operation{
+		Session: session,
+	})
+}
+
+func NewManager(discord *discordgo.Session, sendMessage func(*discordgo.Session, *Session) string) *Manager {
+	manager := &Manager{
+		Sessions:    make(map[string]*Session),
+		Operations:  make([]*Operation, 0),
+		discord:     discord,
+		opmutex:     &sync.Mutex{},
+		sendMessage: sendMessage,
+	}
+
+	go manager.runSendQueue()
+	return manager
+}
